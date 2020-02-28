ec8cf02d139e3437b50a8b81ebc1dd0189a880ad
Eshargh Ghoodjani <e.ghoodjani@gmail.com>
2020-02-28 10:33:56 +0330

    Updates

---
+++ precache-manifest.bb419370499c578a0f175a793a9e9c0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df15c2fec912624a4bdc11c29c0895e",
+    "url": "/MyScrapBook/index.html"
+  },
+  {
+    "revision": "db57b64503257ef8cc5c",
+    "url": "/MyScrapBook/static/css/main.d700c7ee.chunk.css"
+  },
+  {
+    "revision": "fe8b9e761c4c655b8a5b",
+    "url": "/MyScrapBook/static/js/2.1fcc858f.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/MyScrapBook/static/js/2.1fcc858f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db57b64503257ef8cc5c",
+    "url": "/MyScrapBook/static/js/main.037b6607.chunk.js"
+  },
+  {
+    "revision": "dc7785286fed78cb7f7a",
+    "url": "/MyScrapBook/static/js/runtime-main.94a3c824.js"
+  }
+]);
